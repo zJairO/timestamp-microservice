b50417af2f7e2cfab1443abe908e6066afdf67d3
zJairO <hello@zjairo.com>
2023-01-02 15:45:52 -0600

    ready for testing

--- index.js
+++ index.js
@@ -24,9 +24,32 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// date api endpoint
+app.get("/api/:date", (req, res) => {
+  let regex = new RegExp(/^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/);
+  let date;
+  let unix;
+  if (regex.test(req.params.date)) {
+    date = new Date(req.params.date).toUTCString();
+    unix = Math.floor(Date.parse(date));
+  } else {
+    unix = req.params.date;
+    date = new Date(unix * 1000).toUTCString();
+  }
 
+  res.json(
+    {
+      unix: unix,
+      utc: date
+    }
+  )
+});
 
 // listen for requests :)
+/*
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});*/
+var listener = app.listen(8080, () => {
+  console.log('Your app is listening on port '+ listener.address().port);
 });
